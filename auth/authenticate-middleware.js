4822a96193385e04062efefb4db8fda56acbdc28
BaoPham92 <baopham92@yahoo.com>
2019-11-15 13:29:01 -0500

    completed auth for get route

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,27 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    const header = req ?
+        req.headers.authorization :
+        req.connection.context.Authorization;
+
+    console.log('HEADER LOGGING', header, req.headers );
+
+    if (header) {
+        const token = header.replace('Bearer ', '');
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedJWT) => {
+            if (err) {
+                res.status(401).json({
+                    message: "Invalid Credentials",
+                    error: err
+                });
+            } else {
+                req.decodedJWT = decodedJWT;
+                next();
+            }
+        });
+    } else {
+        res.status(400).json({ message: "No credentials provided" });
+    }
 };
--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
-
 const router = require('express').Router();
+const auth = require('../auth/authenticate-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
   const requestOptions = {
     headers: { accept: 'application/json' },
   };
